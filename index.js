95e1908fa09b067edbc8ea3f16d7753492bf901d
elesian <genovacle@hotmail.com>
2022-01-24 15:54:42 +0000

    Project Skeleton for image matching game

---
+++ index.js
@@ -0,0 +1,15 @@
+/* ### A [Memory Matching Game](https://www.memozor.com/memory-games/for-kids/pokemon-game)
+
+- The game starts out with a series of paired images that are hidden from the user.
+- The first 2 images the user clicks on are revealed.
+- If the revelaed images match they remain uncovered.
+- If the images do not match, they are hidden once again.
+- The user continues until they have matched all the images.
+
+_Additional_
+
+- Keep track of how many guesses it took the user to complete the game.
+- Randomise the starting position of the images to stop people cheating.
+
+*/
+
